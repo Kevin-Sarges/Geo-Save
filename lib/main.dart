6c108ad69a705e2f1cb28b4af32feab861e0fe9d
Kevin-Sarges <kevinsarges08@gmail.com>
2024-03-06 11:34:22 -0300

    feat: finalizando nova versão

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geosave/app/common/inject/inject_dependecy.dart';
 import 'package:geosave/app/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   InjectDependecy.init();
   runApp(const MyApp());
